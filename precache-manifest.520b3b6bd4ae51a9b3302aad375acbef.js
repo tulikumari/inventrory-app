d3daade18c86d22a9b21d6a1e679313bf4aadb48
tulikumari <info.tulikumari@gmail.com>
2020-09-09 22:44:16 +0530

    Updates

---
+++ precache-manifest.520b3b6bd4ae51a9b3302aad375acbef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dcf5a99c097d4519239f9468b6fb20e",
+    "url": "/inventrory-app/index.html"
+  },
+  {
+    "revision": "a2c7981ea13e37023e56",
+    "url": "/inventrory-app/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "a2a3a76ca8a2531d9f1c",
+    "url": "/inventrory-app/static/css/main.0e22bf24.chunk.css"
+  },
+  {
+    "revision": "a2c7981ea13e37023e56",
+    "url": "/inventrory-app/static/js/2.40459625.chunk.js"
+  },
+  {
+    "revision": "d3031346469a8a2ef527f1ad705ea3b7",
+    "url": "/inventrory-app/static/js/2.40459625.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2a3a76ca8a2531d9f1c",
+    "url": "/inventrory-app/static/js/main.ee4ee73f.chunk.js"
+  },
+  {
+    "revision": "540191a0216b514bf943",
+    "url": "/inventrory-app/static/js/runtime-main.a0527721.js"
+  }
+]);
